5de87f878ef4585941ce33fac26697fa73faf92e
SAENDJIN <andrey.saenko31@gmail.com>
2022-11-10 15:16:24 +0200

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,9 @@
+from pytest import fixture
+from playwright.sync_api import sync_playwright
+from page_object.deposit import
+from page_object.registration import
+
+@fixture
+def get_playwright():
+    with sync_playwright() as playwright:
+        yield playwright
---
+++ page_object/deposit.py
@@ -0,0 +1,5 @@
+from playwright.sync_api import Playwright
+
+
+class Deposit:
+    def __init__(self, playwright: Playwright):

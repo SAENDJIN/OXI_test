0fee3bab13f23ec415d95f0b036d0ea324603689
SAENDJIN <andrey.saenko31@gmail.com>
2022-11-15 19:00:34 +0200

    add docstring for functions

--- page_object/deposit.py
+++ page_object/deposit.py
@@ -12,23 +12,31 @@ class LoginDeposit:
         self.page.goto("https://oxicasino.io/de/")
 
     def login_form(self):
+        """Fill login inputs"""
         self.page.get_by_role("button", name="Einloggen").click()
         self.page.get_by_placeholder("Benutzername").click()
-        self.page.get_by_placeholder("Benutzername").fill("andrii.qa31+autotest.de@gmail.com")  # Solve what email to use
+        self.page.get_by_placeholder("Benutzername").fill(
+            "andrii.qa31+autotest.de@gmail.com")  # Solve what email to use
         self.page.get_by_placeholder("Passwort").click()
         self.page.get_by_placeholder("Passwort").fill("ilimem31")
 
     def go_to_deposit(self):
+        """Redirect to page with deposit methods"""
         self.page.locator(
-            "form:has-text(\"Benutzername popup_login.field_incorrect Passwort popup_login.field_incorrect Pa\")")\
+            "form:has-text(\"Benutzername popup_login.field_incorrect Passwort popup_login.field_incorrect Pa\")") \
             .get_by_role("button", name="Einloggen").click()
         self.page.get_by_role("button", name="€ 0 Einzahlung").click()
         self.page.wait_for_url("https://oxicasino.io/de/wallet/deposit")
 
     def choose_payment_method(self):
+        """Choose payment method"""
         # add
+
     def check_new_window(self):
+        """Assertion new window and information in it"""
         # add
+
     def close(self):
+        """Close browser and end autotest"""
         self.context.close()
         self.browser.close()

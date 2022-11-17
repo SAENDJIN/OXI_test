99b714e1cb419744584665516b55f4f1ad16f225
SAENDJIN <andrey.saenko31@gmail.com>
2022-11-17 13:21:19 +0200

    done conftest.py

--- conftest.py
+++ conftest.py
@@ -1,9 +1,24 @@
 from pytest import fixture
 from playwright.sync_api import sync_playwright
-from page_object.login import
-from page_object.registration import
+from page_object.login import LoginDeposit
+from page_object.registration import RegistrationDeposit
+
 
 @fixture
 def get_playwright():
     with sync_playwright() as playwright:
         yield playwright
+
+
+@fixture
+def login_deposit_de(get_playwright):
+    login = LoginDeposit(get_playwright)
+    yield login
+    login.close()
+
+
+@fixture
+def registration_deposit_de(get_playwright):
+    registration = RegistrationDeposit(get_playwright)
+    yield registration
+    registration.close()

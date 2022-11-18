cb151bd31be6e55ebc3cdfe2d135dab006ebf170
SAENDJIN <andrey.saenko31@gmail.com>
2022-11-18 15:59:10 +0200

    done birtday_generator.py

--- usefull_tool/birtday_generator.py
+++ usefull_tool/birtday_generator.py
@@ -4,7 +4,4 @@ random_day = random.randint(1, 12)
 random_month = random.randint(1, 12)
 random_year = random.randint(1949, 2000)
 
-# random_date =
-
-
-print(random_day)
+random_date = (str(random_day).zfill(2) + "." + str(random_month).zfill(2) + "." + str(random_year))

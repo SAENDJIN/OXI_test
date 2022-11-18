019fc5b8bb89beb37aa75e187a3134f6679d7644
SAENDJIN <andrey.saenko31@gmail.com>
2022-11-18 12:33:54 +0200

    write def choose_payment_method in login.py

--- usefull_tool/new_phone_generator.py
+++ usefull_tool/new_phone_generator.py
@@ -1,5 +1,11 @@
 import random
 
+random_number = random.randint(0, 9)
+random_number_three = random.randint(100, 999)
+random_number_two = random.randint(10, 99)
 
 "+4 (930) 674-06-06 65"
-random_phone = ("+4 (930) 674-" "06-06 65" + "")
+random_phone = ("+4 (930) " + str(random_number_three) + "-" + str(random_number_two) + "-" + str(
+    random_number_two) + " 65")
+
+print(random_phone)

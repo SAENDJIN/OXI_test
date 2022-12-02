eccf7cd512a02af2c193b1afd14ae20095a30681
SAENDJIN <andrey.saenko31@gmail.com>
2022-12-02 21:57:32 +0200

    uncomment registration_test.py

--- flou_test/registration_test.py
+++ flou_test/registration_test.py
@@ -1,7 +1,11 @@
-# def test_registration_to_deposit(registration_deposit_de):
-#     registration_deposit_de.registration_form()
-#     registration_deposit_de.trigger_profile_input()
-#     registration_deposit_de.profile_info_fill()
-#     registration_deposit_de.deposit_to_redirect()
-#     assert registration_deposit_de.check_new_window()
-#     registration_deposit_de.close()
+import allure
+
+
+@allure.title('Registration new user and make deposit (TRON only)')
+def test_registration_to_deposit(registration_deposit_de):
+    registration_deposit_de.registration_form()
+    registration_deposit_de.trigger_profile_input()
+    registration_deposit_de.profile_info_fill()
+    registration_deposit_de.deposit_to_redirect()
+    assert registration_deposit_de.check_new_window()
+    registration_deposit_de.close()
